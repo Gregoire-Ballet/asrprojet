450866e7ced1f42312e199f9506623f247168397
Gregoire-Ballet <74370859+Gregoire-Ballet@users.noreply.github.com>
2020-12-12 22:42:16 +0100

    Create deconnexion.php

---
+++ deconnexion.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'config/config.inc.php';
+require_once 'config/bdd.conf.php';
+
+setcookie('sid', '', -1);
+
+$_SESSION['notification']['result'] = 'danger';
+$_SESSION['notification']['message'] = 'Vous êtes déconnecté';
+
+header("Location: index.php");
+exit();
